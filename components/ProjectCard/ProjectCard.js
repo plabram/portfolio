d2c0489ad04ec69ff77615f5a72f6d882fdf5b38
Penelope <freyalabram@gmail.com>
2023-06-21 12:26:52 +0200

    Open Projects in new window instead of existing one

--- components/ProjectCard/ProjectCard.js
+++ components/ProjectCard/ProjectCard.js
@@ -2,7 +2,7 @@ import "./ProjectCard.css"
 
 export const projectCard = (project) => `
 <div class="card-text">
-<h3><a href="${project.link}">${project.title}</a></h3>
+<h3><a href="${project.link}" target="_blank">${project.title}</a></h3>
 <p class="stack">${project.stack}</p>
 <p>${project.description}</p>
 </div>
